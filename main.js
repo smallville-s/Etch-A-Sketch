4e97d336ced43e278243c31e3d45539ab5edf0e3
Stefan Silyanov <stefan.silyanov@gmail.com>
2021-06-14 15:53:26 +0300

    Adding Change Grid Size option.

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 const grid = document.getElementById("sketch");
 
-for (let i = 1; i <= 16*16; i++) {
-    const div = document.createElement("div");
-    div.className = "cell";
-    grid.appendChild(div);
+function createGrid(gridSize) {
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    for (let i = 1; i <= gridSize*gridSize; i++) {
+        const div = document.createElement("div");
+        div.className = "cell";
+        div.addEventListener("mouseover", (e) => {
+            div.classList.add("color");
+        });
+        grid.appendChild(div);
+    }
 }
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach(cell => {
-    cell.addEventListener("mouseover", (e) => {
-        cell.classList.add("color");
-    });
-})
+createGrid(16);
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
+const clearBtn = document.getElementById("clearBtn");
+clearBtn.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cell");
     cells.forEach(cell => cell.classList.remove("color"));
 });
+
+const changeGrid = document.getElementById("changeGrid");
+changeGrid.addEventListener("click", setGridSize);
+
+function setGridSize() {
+    let newGridSize = prompt("Enter between 1 and 64 squares per side to make the new grid.");
+    if (newGridSize == null) {
+        return;
+    } else if (newGridSize < 1 || newGridSize > 64 
+            || !Number.isInteger(parseInt(newGridSize))) {
+        setGridSize();    
+    } else {
+    grid.innerHTML="";
+    createGrid(newGridSize);
+    }
+}
